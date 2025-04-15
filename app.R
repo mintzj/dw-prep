cbba3421189449ee95df01471208d0f595a9e179
mintzj <jeff@pacificu.edu>
2025-04-15 17:25:20 -0400

    Basic loader plus str to mainpanel.

--- app.R
+++ app.R
@@ -5,43 +5,43 @@ library(shiny)
 # library(stringr)
 # library(dplyr)
 # library(readr)
-# library(DT)
+library(DT)
 
-# ---- Global Setup ----
-# format_data <- function(raw_dat){
-#   trts <- c("t1", "t2", "t3")
-#   spp <- c("sp1", "sp2")
-#   trials <- c("1", "2")
-# 
-#   colset <- expand.grid(val = c("scale", spp), trt = trts, stage = trials)
-#   cols <- apply(colset, MARGIN = 1, FUN = paste, collapse = ".")
-#   names(raw_dat) <- c("first", "last", "section", cols)
-# 
-#   dat_density <- raw_dat |>
-#     mutate(name = paste(last, first, sep = ", ")) |>
-#     select(!contains("scale"), -first, -last)
-# 
-#   t1 <-  dat_density |>
-#     mutate(trt = "Lemna_common:Salvinia_rare") |>
-#     select(name, section, trt, contains("t1")) 
-#   t2 <-  dat_density |>
-#     mutate(trt = "Lemna50:Salvinia50") |>
-#     select(name, section, trt, contains("t2")) 
-#   t3 <-  dat_density |>
-#     mutate(trt = "Lemna_rare:Salvinia_common") |>
-#     select(name, section, trt, contains("t3")) 
-# 
-#   new_cols <- c("Name", "LabSection", "Treatment", 
-#                 "Lemna_initial", "Salvinia_initial", 
-#                 "Lemna_final", "Salvinia_final")
-# 
-#   names(t3) <- names(t2) <- names(t1) <- new_cols
-#   dat <- rbind(t1, t2, t3) |>
-#     select(1:3, 6, 4, 7, 5) |>
-#     arrange(Name)
-# 
-#   return(dat)
-# }
+
+format_data <- function(raw_dat){
+  trts <- c("t1", "t2", "t3")
+  spp <- c("sp1", "sp2")
+  trials <- c("1", "2")
+
+  colset <- expand.grid(val = c("scale", spp), trt = trts, stage = trials)
+  cols <- apply(colset, MARGIN = 1, FUN = paste, collapse = ".")
+  names(raw_dat) <- c("first", "last", "section", cols)
+
+  dat_density <- raw_dat |>
+    mutate(name = paste(last, first, sep = ", ")) |>
+    select(!contains("scale"), -first, -last)
+
+  t1 <-  dat_density |>
+    mutate(trt = "Lemna_common:Salvinia_rare") |>
+    select(name, section, trt, contains("t1"))
+  t2 <-  dat_density |>
+    mutate(trt = "Lemna50:Salvinia50") |>
+    select(name, section, trt, contains("t2"))
+  t3 <-  dat_density |>
+    mutate(trt = "Lemna_rare:Salvinia_common") |>
+    select(name, section, trt, contains("t3"))
+
+  new_cols <- c("Name", "LabSection", "Treatment",
+                "Lemna_initial", "Salvinia_initial",
+                "Lemna_final", "Salvinia_final")
+
+  names(t3) <- names(t2) <- names(t1) <- new_cols
+  dat <- rbind(t1, t2, t3) |>
+    select(1:3, 6, 4, 7, 5) |>
+    arrange(Name)
+
+  return(dat)
+}
 
 # ---- UI ----
 ui <- fluidPage(
@@ -53,8 +53,8 @@ ui <- fluidPage(
       helpText("Once your data is loaded, a formatted table will appear on the right.")
     ),
     mainPanel(
-      # DT::DTOutput("data_table"),
-      # verbatimTextOutput("file1_contents"),
+      # DTOutput("data_table"),
+      verbatimTextOutput("file1_contents"),
       # downloadButton("downloadData", "Download Formatted Data")
     )
   )
@@ -67,14 +67,14 @@ server <- function(input, output, session) {
     read_csv(input$file1$datapath)
   })
 
-  # formatted_data <- reactive({
-  #   req(raw_data())
-  #   format_data(raw_data())
-  # })
+  formatted_data <- reactive({
+    req(raw_data())
+    format_data(raw_data())
+  })
 
-  # output$file1_contents <- renderPrint({
-  #   str(raw_data())
-  # })
+  output$file1_contents <- renderPrint({
+    str(raw_data())
+  })
 
   # output$data_table <- renderDT({
   #   formatted_data()
